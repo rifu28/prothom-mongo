3b6ed163f1c372472886d3e9702b2eaf480150e5
rifu28 <rifatmeumeu28@gmail.com>
2024-09-09 00:38:47 +0600

    object

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
+app.use(cors());
 
 const users = [
   { id: 1, name: "rifat" },
