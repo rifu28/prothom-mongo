d80f7360e871e7bed3321882ad93e36244ab196e
rifu28 <rifatmeumeu28@gmail.com>
2024-09-08 22:10:09 +0600

    users running

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+
+const users = [
+  { id: 1, name: "rifat" },
+  { id: 1, name: "rifat" },
+  { id: 1, name: "rifat" },
+  { id: 1, name: "rifat" },
+  { id: 1, name: "rifat" },
+];
+
 app.get("/", (req, res) => {
   res.send("users management server is running");
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
 });
 
 app.listen(port, () => {
