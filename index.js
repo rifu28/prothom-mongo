11a76293f28f393d6bc750596693e4061fd12a22
rifu28 <rifatmeumeu28@gmail.com>
2024-09-09 01:40:05 +0600

    post

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 app.use(cors());
+app.use(express.json());
 
 const users = [
   { id: 1, name: "rifat" },
@@ -18,6 +19,14 @@ app.get("/", (req, res) => {
 
 app.get("/users", (req, res) => {
   res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  console.log(req.body);
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(newUser);
 });
 
 app.listen(port, () => {
